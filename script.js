48c033d5320fe4f40f6214a4782eac60e023245b
CharlieDumpitJr <charlie.jr.dumpit@ocsbstudent.ca>
2021-03-23 14:05:34 +0000

    Unit7-01: Random Number Guessing (2)

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ let randomNumber = 0
 let userGuess = 0
 
 function randomizer () {
-  //get number typed in by user and convert to an integer
-  usedGuess = document.getElementById('input').value
+  // get number typed in by user and convert to an integer
+  userGuess = document.getElementById('input').value
   userGuess = parseInt(userGuess)
 
-  //generate random number between 1 and 6
+  // generate random number between 1 and 6
   randomNumber = Math.random() * 6 + 1
   randomNumber = parseInt(randomNumber)
 
-  //compare user's guess with random number 
-  if (userGuess == randomNumber) {
+  // compare user's guess with random number
+  if (userGuess === randomNumber) {
     document.getElementById('answer').innerHTML = 'You Win!'
   }
-} 
+}
